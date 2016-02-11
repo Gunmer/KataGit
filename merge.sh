0c551e341fc959aa2ee366487a37575dc035d4ce
cristiam <cristiam.sosa@innocv.com>
2016-02-11 22:54:11 +0100

    Remove sec on line in father

--- merge.sh
+++ merge.sh
@@ -1,20 +1,17 @@
 git checkout -b merge/padre
-echo "primer commit del padre" > merge_padre.txt
+echo "primer commit del padre" > merge.txt
 git add merge_padre.txt
 git commit -m "primer commit del padre"
 
 git checkout -b merge/hijo
-echo "primer commit de hijo" > merge_hijo.txt
+echo "primer commit de hijo" >> merge.txt
 git add merge_hijo.txt
 git commit -m "primer commit del hijo"
-echo "segundo commit de hijo" >> merge_hijo.txt
+echo "segundo commit de hijo" >> merge.txt
 git add merge_hijo.txt
 git commit -m "segundo commit del hijo"
-echo "tercer commit de hijo" >> merge_hijo.txt
+echo "tercer commit de hijo" >> merge.txt
 git add merge_hijo.txt
 git commit -m "tercer commit del hijo"
 
 git checkout merge/padre
-echo "segundo commit del padre" >> merge_padre.txt
-git add merge_padre.txt
-git commit -m "segundo commit del padre"
