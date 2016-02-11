7ebd3825ae2914ac50cc6c7f1aad1cd4c6859671
cristiam <cristiam.sosa@innocv.com>
2016-02-11 23:10:07 +0100

    Fix merge.sh

--- merge.sh
+++ merge.sh
@@ -1,17 +1,15 @@
 git checkout -b merge/padre
 echo "primer commit del padre" > merge.txt
-git add merge_padre.txt
+git add merge.txt
 git commit -m "primer commit del padre"
 
 git checkout -b merge/hijo
 echo "primer commit de hijo" >> merge.txt
-git add merge_hijo.txt
+git add merge.txt
 git commit -m "primer commit del hijo"
 echo "segundo commit de hijo" >> merge.txt
-git add merge_hijo.txt
+git add merge.txt
 git commit -m "segundo commit del hijo"
 echo "tercer commit de hijo" >> merge.txt
-git add merge_hijo.txt
+git add merge.txt
 git commit -m "tercer commit del hijo"
-
-git checkout merge/padre
